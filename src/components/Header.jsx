75b4b9cb6c0e3422d350777628b4daeba239667c
AnAtoliyAK <aliaksandrau89@gmail.com>
2020-11-11 22:32:50 +0300

    feat: add first page

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+        <img src="https://i.pinimg.com/736x/5a/02/5e/5a025e222970a3dd2c3706bde935ae15.jpg"></img>
+      </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://s.yimg.com/ny/api/res/1.2/12UU2JphAsbxTTDca.7QFQ--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9MTA4MDtoPTcxNg--/https://media-mbst-pub-ue1.s3.amazonaws.com/creatr-uploaded-images/2019-11/7b5b5330-112b-11ea-a77f-7c019be7ecae"></img>
+            </div>
+
+            <div>
+                <img src="https://i.pinimg.com/originals/46/da/e5/46dae512e375bee2664a025507da8795.jpg"></img>
+            </div>
+            <div>My posts</div>
+            <div>Post 1</div>
+            <div>Post 2</div>
+        </div>
+    )
+}
+
+export default Profile;
