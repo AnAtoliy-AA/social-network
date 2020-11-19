7ef273d6f66bb6de818758a11f102ada6216462e
AnAtoliyAK <aliaksandrau89@gmail.com>
2020-11-20 02:51:53 +0300

    feat: add navlinks to dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,23 +1,42 @@
+import { NavLink } from 'react-router-dom';
 import React from 'react';
 import styles from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
         <div className={styles.dialogs}>
-           <div className={ styles.dialogsItems }>
-               <div className={ styles.dialog }>SASHA</div>
-               <div className={ styles.dialog }>MASHA</div>
-               <div className={ styles.dialog }>PASHA</div>
-               <div className={ styles.dialog }>KASHA</div>
-               <div className={ styles.dialog }>PROSTOKVASHA</div>
-           </div>
-           <div className={ styles.messages }>
-               <div className={ styles.message }>hi</div>
-               <div className={ styles.message }>ghg</div>
-               <div className={ styles.message }>dsssssssss</div>
-               <div className={ styles.message }>sdfsdfsfd</div>
-               <div className={ styles.message }>sdrt</div>
-           </div>
+            <div className={styles.dialogsItems}>
+                <div className={styles.dialog}>
+                    <NavLink to="/dialogs/1">
+                        SASHA
+                       </NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to="/dialogs/2">
+                        MASHA</NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to="/dialogs/3">
+                        PASHA
+                        </NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to="/dialogs/4">
+                        KASHA </NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to="/dialogs/5">
+                        PROSTOKVASHA
+                         </NavLink>
+                </div>
+            </div>
+            <div className={styles.messages}>
+                <div className={styles.message}>hi</div>
+                <div className={styles.message}>ghg</div>
+                <div className={styles.message}>dsssssssss</div>
+                <div className={styles.message}>sdfsdfsfd</div>
+                <div className={styles.message}>sdrt</div>
+            </div>
         </div>
     )
 }
