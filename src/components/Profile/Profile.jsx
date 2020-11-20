0aedab666e227dec96db6b094ce3ff0f2cb1619d
AnAtoliyAK <aliaksandrau89@gmail.com>
2020-11-20 03:19:14 +0300

    refactor: add profile info component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import MyPosts from './MyPosts/MyPosts';
+import ProfileInfo from './ProfileInfo/ProfileInfo';
 import React from 'react';
 
 // import styles from './Profile.module.css';
@@ -6,13 +7,7 @@ import React from 'react';
 const Profile = () => {
     return (
         <div>
-            <div>
-                <img src="https://s.yimg.com/ny/api/res/1.2/12UU2JphAsbxTTDca.7QFQ--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9MTA4MDtoPTcxNg--/https://media-mbst-pub-ue1.s3.amazonaws.com/creatr-uploaded-images/2019-11/7b5b5330-112b-11ea-a77f-7c019be7ecae" alt="start screen"></img>
-            </div>
-
-            {/* <div>
-                <img src="https://i.pinimg.com/originals/46/da/e5/46dae512e375bee2664a025507da8795.jpg"></img>
-            </div> */}
+           <ProfileInfo />
            <MyPosts />
         </div>
     )
