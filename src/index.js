f14b9ed926a48f98a659c52ed24bfc63e0837904
AnAtoliyAK <aliaksandrau89@gmail.com>
2020-11-20 21:31:31 +0300

    refactor: add state js

--- src/index.js
+++ src/index.js
@@ -5,29 +5,11 @@ import * as serviceWorker from './serviceWorker';
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-let posts = [
-  { id: 1, message: 'hi', likesCount: 4 },
-  { id: 2, message: 'hi how are you', likesCount: 6 },
-];
-
-let dialogs = [
-  { id: 1, name: 'SASHA' },
-  { id: 2, name: 'MASHA' },
-  { id: 3, name: 'PASHA' },
-  { id: 4, name: 'KASHA' },
-  { id: 5, name: 'PROSTOKVASHA' },
-];
-
-let messages = [
-  { id: 1, message: 'hi' },
-  { id: 2, message: 'by' },
-  { id: 3, message: 'fdf' },
-];
+import state from './redux/state';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App posts={posts} dialogs={dialogs} messages={messages} />
+    <App state={state} />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/redux/state.js
@@ -0,0 +1,25 @@
+let state = {
+    profilePage: {
+        posts: [
+            { id: 1, message: 'hi', likesCount: 4 },
+            { id: 2, message: 'hi how are you', likesCount: 6 },
+        ],
+    },
+    dialogsPage:{
+        dialogs: [
+            { id: 1, name: 'SASHA' },
+            { id: 2, name: 'MASHA' },
+            { id: 3, name: 'PASHA' },
+            { id: 4, name: 'KASHA' },
+            { id: 5, name: 'PROSTOKVASHA' },
+        ],
+        messages: [
+            { id: 1, message: 'hi' },
+            { id: 2, message: 'by' },
+            { id: 3, message: 'fdf' },
+        ],
+    }
+    
+}
+
+export default state;
