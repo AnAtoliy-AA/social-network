363bcc24169357014d5426578134f6e24f880299
AnAtoliyAK <aliaksandrau89@gmail.com>
2020-12-01 19:55:33 +0300

    refactor: profile reducer

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -19,16 +19,17 @@ const profileReducer = (state = initialState, action) => {
                 message: state.newPostText,
                 likesCount: 0,
             };
-            let stateCopy = { ...state };
-            stateCopy.posts = [...state.posts];
-            stateCopy.posts.push(newPost);
-            stateCopy.newPostText = '';
-            return stateCopy;
+            return {
+                ...state,
+                posts: [...state.posts, newPost],
+                newPostText: ''
+            };
         }
         case ACTION_CONST.UPDATE_NEW_POST_TEXT:
-            let stateCopy = {...state};
-            stateCopy.newPostText = action.newPostText;
-            return stateCopy;
+            return {
+                ...state,
+                newPostText: action.newPostText
+            }
         default:
             return state;
     }
