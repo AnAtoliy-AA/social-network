0a931d0d1c7696166fc9cd9601a7e5f2fbee28e4
AnAtoliyAK <aliaksandrau89@gmail.com>
2020-12-29 21:42:23 +0300

    feat: add local state to profile status

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,4 +1,5 @@
 import Preloader from '../../common/Preloader/Preloader'
+import ProfileStatus from './ProfileStatus';
 import React from 'react';
 import styles from './ProfileInfo.module.css';
 
@@ -9,12 +10,12 @@ if (!props.profile) {
 
     return (
         <div>
-            <div>
+            {/* <div>
                 <img className={styles.main__img} src="https://s.yimg.com/ny/api/res/1.2/12UU2JphAsbxTTDca.7QFQ--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9MTA4MDtoPTcxNg--/https://media-mbst-pub-ue1.s3.amazonaws.com/creatr-uploaded-images/2019-11/7b5b5330-112b-11ea-a77f-7c019be7ecae" alt="start screen"></img>
-            </div>
+            </div> */}
             <div className={styles.descriptionBlock}>
                 <img src={props.profile.photos.large} alt=""/>
-                ava+discription
+                <ProfileStatus status={'zz'}/>
             </div>
         </div>
     )
