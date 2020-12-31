5ee3e76360feb8bdcafd2ea123031ba035e80f12
AnAtoliyAK <aliaksandrau89@gmail.com>
2020-12-31 16:54:50 +0300

    feat: add logout and login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,11 @@ const Header = (props) => {
         alt="logo"></img>
 
       <div className={classes.loginBlock}>
-        {props.isAuth ? props.login
+        {props.isAuth
+          ? <div>
+            {props.login} - 
+            <button onClick={props.logout}>Log out</button>
+          </div>
           : <NavLink to={'login'}>Login</NavLink>}
       </div>
     </header>
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,28 +1,35 @@
 import { Field, reduxForm } from 'redux-form'
+import { login, logout } from '../../redux/auth-reducer'
 import { maxLengthCreator, required } from '../../utils/validators/validators';
 
 import { Element } from '../common/FormsControls/FormsControls';
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const maxLength50 = maxLengthCreator(50);
+const MAX_LENGTH = 50;
+
+const maxLength = maxLengthCreator(MAX_LENGTH);
 const Input = Element("input");
+
 const LoginForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
                 <Field
-                    name={'login'}
-                    placeholder={'Login'}
+                    name={'email'}
+                    placeholder={'Email'}
                     component={Input}
-                    validate={[required, maxLength50]}
+                    validate={[required, maxLength]}
                 />
             </div>
             <div>
                 <Field
                     name={'password'}
                     placeholder={'Password'}
+                    type={'password'}
                     component={Input}
-                    validate={[required, maxLength50]}
+                    validate={[required, maxLength]}
                 />
             </div>
             <div>
@@ -43,15 +50,23 @@ const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm)
 
 const Login = (props) => {
     const onSubmit = (formData) => {
-        console.log('FORM_DATA: ', formData);
+        props.login(formData.email, formData.password, formData.rememberMe)
     }
+    if (props.isAuth) {
+        return <Redirect to={'profile'} />
+    }
+
     return (
         <div>
             <h1>Login</h1>
-            <LoginReduxForm onSubmit={onSubmit}/>
+            <LoginReduxForm onSubmit={onSubmit} />
         </div>
     )
 }
 
+const mapStateToProps = (state) => ({
+isAuth: state.auth.isAuth
+})
 
-export default Login;
+
+export default connect(mapStateToProps, { login })(Login);
